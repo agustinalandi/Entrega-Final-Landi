22e495356dd37d67f5bd371593aefef724632b8f
Agustina Landi <agustinalandi2@gmail.com>
2022-06-26 23:11:47 -0300

    admin changes

--- ropa/admin.py
+++ ropa/admin.py
@@ -2,7 +2,14 @@ from django.contrib import admin
 from ropa.models import Accesorio, Pedido, Prenda
 
 # Register your models here.
+@admin.register(Pedido)
+class PedidoAdmin(admin.ModelAdmin):
+    list_display = ['prenda', 'fecha_pedido', 'precio', 'es_temporada_actual']
 
-admin.site.register(Pedido)
-admin.site.register(Prenda)
-admin.site.register(Accesorio)
+@admin.register(Prenda)
+class PrendaAdmin(admin.ModelAdmin):
+    list_display = ['nombre', 'talle', 'color']
+
+@admin.register(Accesorio)
+class AccesorioAdmin(admin.ModelAdmin):
+    list_display = ['tipo', 'talle_accesorio', 'color_accesorio', 'es_para_regalo']
