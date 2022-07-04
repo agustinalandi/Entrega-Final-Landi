ea43de12b9f877aa316f6514106cb6dd206e9eb1
Agustina Landi <agustinalandi2@gmail.com>
2022-07-04 12:56:33 -0300

    about

--- ropa/views.py
+++ ropa/views.py
@@ -15,8 +15,8 @@ from ropa.forms import Pedido_form, Prenda_form
 def index(request):
     return render(request, 'index.html')
 
-def contacto(request):
-    return render(request, 'contacto.html')
+def nosotros(request):
+    return render(request, 'nosotros.html')
 
 class Listar_prendas(ListView):
     model = Prenda
--- tienda/urls.py
+++ tienda/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-from ropa.views import index, contacto
+from ropa.views import index, nosotros
 
 from django.conf import settings
 from django.conf.urls.static import static
@@ -8,7 +8,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', index, name = 'index'),
     path('admin/', admin.site.urls),
-    path('contacto/', contacto, name = 'contacto'),
+    path('about/', nosotros, name = 'nosotros'),
     
     path('ropa/', include('ropa.urls')),
     path('accesorios/', include('accesorios.urls')),
